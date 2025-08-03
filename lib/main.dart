5b14892ce11b628b955bbe8b75f5939215178a42
awalhsnmunna <awalhsnmunna@gmail.com>
2025-08-03 19:08:39 +0600

    Expanded|Stack|Positioned

--- lib/main.dart
+++ lib/main.dart
@@ -1,224 +1,74 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({super.key});
-=======
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); 
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.green,
-        appBarTheme: AppBarTheme(color: Colors.red, centerTitle: true),
-        scaffoldBackgroundColor: Colors.white,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.green,
-            foregroundColor: Colors.white,
-          ),
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-      title: "Flutter App",
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    TextEditingController _emailController = TextEditingController();
-    TextEditingController _passwordController = TextEditingController();
-    TextEditingController _numberController = TextEditingController();
-
-    final _formKey = GlobalKey<FormState>();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Column(
+      home: Scaffold(
+        body: Column(
           children: [
-            Text(
-              "Ostad Flutter 2",
-              style: TextStyle(fontSize: 25, color: Colors.white),
+            Center(
+              child: SizedBox(
+                height: 100,
+                width: 250,
+                child: ColoredBox(color: Colors.green),
+              ),
             ),
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          Form(
-            key: _formKey,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
+            SizedBox(
+              height: 500,
+              width: 200,
               child: Column(
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Email",
-                      label: Text("Email"),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Empty email is not allowed";
-                      }
-                      return null;
-                    },
-                  ),
+                  Expanded(flex: 1, child: Container(color: Colors.blue)),
 
-                  SizedBox(height: 20),
-
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Password",
-                      label: Text("Password"),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty || value.length < 6) {
-                        return "Password must be 6 characters long";
-                      }
-                      return null;
-                    },
-                  ),
-
-                  SizedBox(height: 20),
-
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                      ),
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => UserInfo()),
-                          );
-
-                          // Navigator.pushReplacement(
-                          //   context,
-                          //   MaterialPageRoute(builder: (context) => UserInfo()),
-                          // );
-                        }
-                      },
-                      child: Text(
-                        "Submit",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height: 20),
-
-                  //GridView
-                  SizedBox(
-                    height: 300,
-                    child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 5,
-                      ),
-                      itemCount: 7,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Center(
-                            child: Icon(
-                              Icons.mobile_friendly,
-                              size: 35,
-                              color: Colors.white,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
+                  Expanded(flex: 7, child: Container(color: Colors.amber)),
                 ],
               ),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          print("Floating action button pressed");
-        },
-        label: Icon(Icons.add),
-      ),
-    );
-  }
-}
 
-class UserInfo extends StatelessWidget {
-  const UserInfo({super.key});
+            SizedBox(height: 10),
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("User Page")),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text("Back"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Page1()),
-              );
-            },
-            child: Text("Page1"),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Page1 extends StatelessWidget {
-  const Page1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Page 1")),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text("Back"),
-          ),
-          /*ElevatedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=> Page1()));
-            }, child: Text("Page1")), */
-        ],
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                Positioned(
+                  //top: 10,
+                  //left: 10,
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
